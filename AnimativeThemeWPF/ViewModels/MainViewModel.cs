97df4d9dbab6620027e50ea7fbc8cca41cee9f37
jrd <kettlesimzulator420@gmail.com>
2020-11-13 13:12:55 +0000

    Add project files.

---
+++ AnimativeThemeWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using AnimativeThemeWPF.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimativeThemeWPF.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ObservableCollection<TestItemViewModel> TestItems { get; set; }
+
+        public MainViewModel()
+        {
+            TestItems = new ObservableCollection<TestItemViewModel>();
+
+            TestItems.Add(new TestItemViewModel() { ProductName = "Xbox One", ProductDescription = "This product is ok.", ProductPrice = 200.99 });
+            TestItems.Add(new TestItemViewModel() { ProductName = "Playstation 4", ProductDescription = "a", ProductPrice = 250.99 });
+            TestItems.Add(new TestItemViewModel() { ProductName = "PC", ProductDescription = "AMD, AMD, AMD, AMD, AMD and Samsung", ProductPrice = 749.99 });
+            TestItems.Add(new TestItemViewModel() { ProductName = "Yacht", ProductDescription = "Fast", ProductPrice = 450000.99 });
+            TestItems.Add(new TestItemViewModel() { ProductName = "Bob the builder's antimatter generator", ProductDescription = "Only uses 193.6 terajoules per second.", ProductPrice = 999999.99 });
+            TestItems.Add(new TestItemViewModel() { ProductName = "Playstation 4", ProductDescription = "repeating because h", ProductPrice = 250.99 });
+
+        }
+    }
+}
---
+++ AnimativeThemeWPF/ViewModels/TestItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using AnimativeThemeWPF.Utilities;
+
+namespace AnimativeThemeWPF.ViewModels
+{
+    public class TestItemViewModel : BaseViewModel
+    {
+        private string _productName;
+        public string ProductName
+        {
+            get => _productName;
+            set => RaisePropertyChanged(ref _productName, value);
+        }
+
+        private string _productDescription;
+        public string ProductDescription
+        {
+            get => _productDescription;
+            set => RaisePropertyChanged(ref _productDescription, value);
+        }
+
+        private double _productPrice;
+        public double ProductPrice
+        {
+            get => _productPrice;
+            set => RaisePropertyChanged(ref _productPrice, value);
+        }
+    }
+}
